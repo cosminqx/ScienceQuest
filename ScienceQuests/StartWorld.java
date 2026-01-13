13267500edf8aabd469340c760e4f8695a27a3a1
Silviu Chiscareanu <schiscareanu@yahoo.com>
2026-01-13 18:03:54 +0200

    New Background image added

--- ScienceQuests/StartWorld.java
+++ ScienceQuests/StartWorld.java
@@ -12,7 +12,19 @@ public class StartWorld extends World
     public StartWorld()
     {    
         super(600, 400, 1);  // width, height, cell size
+        setScaledBackground();
         showTitleScreen();
+    }
+
+    /**
+     * Scale and set the background image to fit the world
+     */
+    private void setScaledBackground()
+    {
+        GreenfootImage bg =
+            new GreenfootImage("pngtree-nobody-interface-of-pixel-game-platform-picture-image_1962988.jpg");
+        bg.scale(getWidth(), getHeight());
+        setBackground(bg);
     }
 
     /**
@@ -31,11 +43,9 @@ public class StartWorld extends World
         removeAllObjects();
         currentScreen = 0;
 
-        // Title
         Label title = new Label("ScienceQuests", 48);
         addObject(title, getWidth()/2, 120);
 
-        // Start Button
         StartButton startButton = new StartButton();
         addObject(startButton, getWidth()/2, 250);
     }
@@ -45,19 +55,15 @@ public class StartWorld extends World
         removeAllObjects();
         currentScreen = 1;
 
-        // Title
         Label title = new Label("Create Your Character", 36);
         addObject(title, getWidth()/2, 50);
 
-        // Name label
         Label nameLabel = new Label("Enter your name:", 24);
         addObject(nameLabel, getWidth()/2, 130);
 
-        // Name input field
         nameField = new InputField(300, 40, 20);
         addObject(nameField, getWidth()/2, 180);
 
-        // Continue button
         continueButton = new StartButton() 
         {
             public void startGame()
@@ -73,11 +79,9 @@ public class StartWorld extends World
         removeAllObjects();
         currentScreen = 2;
 
-        // Title
         Label title = new Label("Select your gender:", 36);
         addObject(title, getWidth()/2, 50);
 
-        // Gender buttons
         maleButton = new GenderButton("Male");
         addObject(maleButton, 150, 180);
 
@@ -87,7 +91,6 @@ public class StartWorld extends World
         otherButton = new GenderButton("Other");
         addObject(otherButton, getWidth() - 150, 180);
 
-        // Continue button
         continueButton = new StartButton()
         {
             public void startGame()
@@ -109,27 +112,16 @@ public class StartWorld extends World
 
     private void proceedToGame()
     {
-        if (maleButton != null && femaleButton != null && otherButton != null)
-        {
-            if (maleButton.isSelected())
-            {
-                PlayerData.setPlayerGender("Male");
-            }
-            else if (femaleButton.isSelected())
-            {
-                PlayerData.setPlayerGender("Female");
-            }
-            else if (otherButton.isSelected())
-            {
-                PlayerData.setPlayerGender("Other");
-            }
-            else
-            {
-                return; // No gender selected
-            }
+        if (maleButton.isSelected())
+            PlayerData.setPlayerGender("Male");
+        else if (femaleButton.isSelected())
+            PlayerData.setPlayerGender("Female");
+        else if (otherButton.isSelected())
+            PlayerData.setPlayerGender("Other");
+        else
+            return;
 
-            Greenfoot.setWorld(new MainMapWorld());
-        }
+        Greenfoot.setWorld(new MainMapWorld());
     }
 
     public void showNameScreen()
