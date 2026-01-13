17b2465ecd018bbfaf4f8478bccd16064fb6f72a
Sebastian Cosmin Lăcătuș <cosmin.lacatus@outlook.com>
2026-01-13 16:22:00 +0200

    Update MainMapWorld to display player greeting and gender information

--- ScienceQuests/MainMapWorld.java
+++ ScienceQuests/MainMapWorld.java
@@ -5,8 +5,22 @@ public class MainMapWorld extends World
     public MainMapWorld()
     {
         super(600, 400, 1);
-
-        Label label = new Label("Main Map (Prototype)", 30);
-        addObject(label, getWidth()/2, getHeight()/2);
+        
+        // Retrieve player data
+        String playerName = PlayerData.getPlayerName();
+        String playerGender = PlayerData.getPlayerGender();
+        
+        // Display greeting with player name
+        Label greetingLabel = new Label("Hello, " + playerName + "!", 32);
+        addObject(greetingLabel, getWidth()/2, 100);
+        
+        // Display gender information
+        Label genderLabel = new Label("You are gender of " + playerGender + ".", 24);
+        addObject(genderLabel, getWidth()/2, 150);
+        
+        // Main map placeholder
+        Label mapLabel = new Label("Main Map (Prototype)", 30);
+        addObject(mapLabel, getWidth()/2, getHeight()/2);
     }
 }
+
