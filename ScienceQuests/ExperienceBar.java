9a9a08377a88a660a4a9ae516c470884d6631727
Sebastian Cosmin Lăcătuș <cosmin.lacatus@outlook.com>
2026-02-03 16:10:23 +0200

    Remove text below bar and adjust LVL text position for better vertical centering

--- ScienceQuests/ExperienceBar.java
+++ ScienceQuests/ExperienceBar.java
@@ -8,8 +8,6 @@ public class ExperienceBar extends Actor
 {
     private static final int BAR_WIDTH = 200;
     private static final int BAR_HEIGHT = 20;
-    private static final int LEVEL_TEXT_HEIGHT = 20;
-    private static final int TOTAL_HEIGHT = BAR_HEIGHT + LEVEL_TEXT_HEIGHT;
     private static final int BORDER_WIDTH = 2;
     private static final Color BORDER_COLOR = Color.BLACK;
     private static final Color BG_COLOR = new Color(40, 40, 40);
@@ -41,7 +39,7 @@ public class ExperienceBar extends Actor
     
     /**
      * Redraw the XP bar based on current XP percentage
-     * Displays level text in top-left corner of bar and "Level X" below the bar
+     * Displays level text in top-left corner of bar
      */
     private void updateImage()
     {
@@ -49,7 +47,7 @@ public class ExperienceBar extends Actor
         float xpPercent = state.getXPPercent();
         int level = state.getLevel();
         
-        GreenfootImage img = new GreenfootImage(BAR_WIDTH, TOTAL_HEIGHT);
+        GreenfootImage img = new GreenfootImage(BAR_WIDTH, BAR_HEIGHT);
         
         // Draw background
         img.setColor(BG_COLOR);
@@ -65,23 +63,16 @@ public class ExperienceBar extends Actor
         img.drawRect(0, 0, BAR_WIDTH - 1, BAR_HEIGHT - 1);
         img.drawRect(1, 1, BAR_WIDTH - 3, BAR_HEIGHT - 3);
         
-        // Draw level text in top-left corner of bar
+        // Draw level text in top-left corner of bar (adjusted 5px lower for better centering)
         img.setFont(FontManager.getPixeledSmall());
         img.setColor(Color.WHITE);
         String levelText = "LVL " + level;
-        img.drawString(levelText, 4, 10);
+        img.drawString(levelText, 4, 15);
         
         // Draw XP text in center
         String xpText = state.getXp() + "/" + state.getMaxXP();
         int textX = (BAR_WIDTH - (xpText.length() * 6)) / 2;
         img.drawString(xpText, textX, BAR_HEIGHT - 5);
-        
-        // Draw "Level X" text below the bar
-        img.setFont(FontManager.getPixeled());
-        img.setColor(Color.WHITE);
-        String levelDisplayText = "Level " + level;
-        int levelDisplayX = (BAR_WIDTH - (levelDisplayText.length() * 8)) / 2;
-        img.drawString(levelDisplayText, levelDisplayX, BAR_HEIGHT + 16);
         
         setImage(img);
     }
