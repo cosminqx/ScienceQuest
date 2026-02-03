52e7bc8627e544f8ade4b9b95326465f77bb3079
Sebastian Cosmin Lăcătuș <cosmin.lacatus@outlook.com>
2026-02-03 16:03:24 +0200

    Add 'Level X' text display below experience bar

--- ScienceQuests/ExperienceBar.java
+++ ScienceQuests/ExperienceBar.java
@@ -8,6 +8,8 @@ public class ExperienceBar extends Actor
 {
     private static final int BAR_WIDTH = 200;
     private static final int BAR_HEIGHT = 20;
+    private static final int LEVEL_TEXT_HEIGHT = 20;
+    private static final int TOTAL_HEIGHT = BAR_HEIGHT + LEVEL_TEXT_HEIGHT;
     private static final int BORDER_WIDTH = 2;
     private static final Color BORDER_COLOR = Color.BLACK;
     private static final Color BG_COLOR = new Color(40, 40, 40);
@@ -39,7 +41,7 @@ public class ExperienceBar extends Actor
     
     /**
      * Redraw the XP bar based on current XP percentage
-     * Displays level in top-left corner of the bar
+     * Displays level text in top-left corner of bar and "Level X" below the bar
      */
     private void updateImage()
     {
@@ -47,7 +49,7 @@ public class ExperienceBar extends Actor
         float xpPercent = state.getXPPercent();
         int level = state.getLevel();
         
-        GreenfootImage img = new GreenfootImage(BAR_WIDTH, BAR_HEIGHT);
+        GreenfootImage img = new GreenfootImage(BAR_WIDTH, TOTAL_HEIGHT);
         
         // Draw background
         img.setColor(BG_COLOR);
@@ -73,6 +75,13 @@ public class ExperienceBar extends Actor
         String xpText = state.getXp() + "/" + state.getMaxXP();
         int textX = (BAR_WIDTH - (xpText.length() * 6)) / 2;
         img.drawString(xpText, textX, BAR_HEIGHT - 5);
+        
+        // Draw "Level X" text below the bar
+        img.setFont(FontManager.getPixeled());
+        img.setColor(Color.WHITE);
+        String levelDisplayText = "Level " + level;
+        int levelDisplayX = (BAR_WIDTH - (levelDisplayText.length() * 8)) / 2;
+        img.drawString(levelDisplayText, levelDisplayX, BAR_HEIGHT + 16);
         
         setImage(img);
     }
